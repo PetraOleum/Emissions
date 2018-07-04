b97d4e01d96dc9be96e2010cb946249659de8b5e
Petra Lamborn <oleumpetra@gmail.com>
2018-07-04 22:07:17 +1200

    Basic graph

--- app.R
+++ app.R
@@ -8,23 +8,61 @@ Emissions.Sector <- read_excel("Emissions1990-2010.xlsx",
                                         sheet = "Sector")
 Emissions.Fuel <- read_excel("Emissions1990-2010.xlsx", 
                              sheet = "Fuel")
+Sector.Tot <- filter(Emissions.Sector, Source == "Total")
 
-# Define UI for application that draws a histogram
+sectors.a <- unique(Emissions.Sector$Sector)
+sectors <- unique(Emissions.Sector$Sector)[-1]
+
+# gather(comf, key="Year", value="Co2eqv", as.character(1990:2010))
+
+Sector.Fuels <- function(sector) {
+  sf <- filter(Emissions.Sector,  Sector == sector)
+  if (nrow(sf) > 1) {
+    sf <- filter(sf, !grepl("Total", Source))
+  }
+  return(sf)
+}
+
+
+
+# UI ----
 ui <- fluidPage(
    
-   titlePanel("NZ emissions 1990-2010"),
+   titlePanel("NZ greenhouse gas emissions 1990-2010"),
    
    sidebarLayout(
+      # Sidebar ----
       sidebarPanel(
       ),
       
+      # Main panel ----
       mainPanel(
+        tabsetPanel(id="tabs",
+                   tabPanel("by-sector", 
+                            plotOutput("secPlot")
+                            )
+                    )
       )
    )
 )
 
 server <- function(input, output) {
+  SecOverall <- reactive({
+    st <- gather(Sector.Tot, key="Year",
+                 value="Co2eqv", as.character(1990:2010)) %>% 
+      mutate(Year = as.numeric(Year), 
+             Sector = factor(Sector, levels=unique(Sector)))
+    # Add logic for filtering out years, sectors here
+    return(st)
+  })
    
+  output$secPlot <- renderPlot({
+    sov <- SecOverall()
+    ggplot(sov, aes(x=Year, y=Co2eqv, linetype=Sector, color=Sector)) + 
+      geom_line(size=1.5) + guides(linetype=guide_legend(keywidth = 3)) +
+      scale_y_log10() -> s.p
+    return(s.p)
+  })
 }
 
 shinyApp(ui = ui, server = server)
